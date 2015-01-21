a3212cbcc8a407b409741d9ead741215aeaf2d44
Adrien <asiegfried.enac@gmail.com>
2015-01-21 17:19:57 +0100

    Changing package

--- src/markingmenu/PieItem.java
+++ src/markingmenu/PieItem.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package piemenu;
+package markingmenu;
 
 import java.awt.Color;
 import java.awt.Dimension;
