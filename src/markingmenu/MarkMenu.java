650159bc542c646db58b5211a808b9c506788e56
Jonas Gouraud <jonasg@live.fr>
2015-02-16 13:43:14 +0100

    ajout du diagramme de classe et des licences

--- src/markingmenu/MarkMenu.java
+++ src/markingmenu/MarkMenu.java
@@ -1,11 +1,23 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2015 Adrien Siegfried Jonas Gouraud
+ * 
+ * This file is part of MarkingMenu.
+ *
+ * MarkingMenu is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * MarkingMenu is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MarkingMenu. If not, see <http://www.gnu.org/licenses/>.
  */
 package markingmenu;
 
-import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Point;
 import java.awt.event.MouseAdapter;
